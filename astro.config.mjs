73adf69c276aa0af072cff3df6aafed5ad710644
JatinPrajapat <jatinprajapat028@gmail.com>
2026-06-10 21:29:03 +0530

    FIX: ASTRO CONFIG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone'
-  }),
-
   integrations: [react()],
 
   vite: {
